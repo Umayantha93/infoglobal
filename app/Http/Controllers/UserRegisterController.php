2936b0afbeb594e8431000957a4f49871d82edd7
Umayantha93 <umayantha93@gmail.com>
2021-10-22 10:57:23 +0530

    search any

--- app/Http/Controllers/UserRegisterController.php
+++ app/Http/Controllers/UserRegisterController.php
@@ -3,21 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserRegister;
 
 class UserRegisterController extends Controller
 {
     //
 
-    public function register(Request $request)
+    public function Register(Request $request)
     {
-        $user_reg = new User();
 
-        $user_reg->name = $request->name;
-        $user_reg->email = $request->email;
-        $user_reg->password = bcrypt($request->password);
-        $user_reg->status = $request->status;
-        $user_reg->is_admin = $request->is_admin;
+        // $uploaded_files = $request->file->store('public/uploads');
 
-        $user->save();
+        $user_reg = new UserRegister();
+
+        $user_reg->id_number = $request->id_number;
+        $user_reg->person_name = $request->person_name;
+        $user_reg->date_of_birth = $request->date_of_birth;
+        $user_reg->contact_number = $request->contact_number;
+        $user_reg->age = $request->age;
+        $user_reg->address = $request->address;
+        $user_reg->religion = $request->religion;
+        $user_reg->nationality = $request->nationality;
+        $user_reg->image_path = $request->image_path;
+  
+        $user_reg->save();
+
+        return response()->json([
+            'message' => "succesfully uploaded"
+        ]);
+    }
+
+    public function list(){
+        $user = UserRegister::all('id_number','person_name','date_of_birth','contact_number','age','address'
+        ,'religion','nationality','image_path');
+
+        return $user;
+    }
+
+    public function search($search){
+
+        return UserRegister::where("person_name","like","%".$search."%")->
+        orWhere("address","like","%".$search."%")->
+        orWhere("id_number", "like", "%".$search."%")->
+        orWhere("date_of_birth","like","%".$search."%")->
+        orWhere("contact_number", "like", "%".$search."%")->
+        orWhere("age","like","%".$search."%")->
+        orWhere("religion", "like", "%".$search."%")->
+        orWhere("nationality", "like", "%".$search."%")
+        ->get();
+    }
+
+    public function dashboard(){
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserRegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', [UserRegisterController::class, "Register"]);
+Route::get('get', [UserRegisterController::class, "list"]);
+Route::get('search/{search}', [UserRegisterController::class, "search"]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
