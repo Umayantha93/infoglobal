83c5b1f3fc1ffd1e7f1c81595fa67e78639666d0
Umayantha93 <umayantha93@gmail.com>
2021-10-30 14:35:48 +0530

    backend

--- app/Http/Controllers/UserRegisterController.php
+++ app/Http/Controllers/UserRegisterController.php
@@ -4,17 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserRegister;
+use Intervention\Image\ImageServiceProvider;
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\Builder;
+
 
 class UserRegisterController extends Controller
 {
     //
 
-    public function Register(Request $request)
+    public function register(Request $request)
     {
 
         // $uploaded_files = $request->file->store('public/uploads');
 
+        error_log($request);
+
         $user_reg = new UserRegister();
+
+        error_log($request);
 
         $user_reg->id_number = $request->id_number;
         $user_reg->person_name = $request->person_name;
@@ -24,24 +33,42 @@ class UserRegisterController extends Controller
         $user_reg->address = $request->address;
         $user_reg->religion = $request->religion;
         $user_reg->nationality = $request->nationality;
-        $user_reg->image_path = $request->image_path;
+        if($request->hasFile("image_path")){
+            $img = $request->image_path;
+            $img_name = time().'-'.$img->getClientOriginalName();
+            Image::make($img)->save(storage_path("app/public/".$img_name));
+            $user_reg->image_path = $img_name;
+        }
+        // $user_reg->image_path = $request->image_path;
   
-        $user_reg->save();
+        if($user_reg->save()){
+            return response()->json([
+                "data" => $user_reg,
+                'message' => "succesfully uploaded"
+            ]);
+        }else{
+            return response()->json([
+                "data" => null,
+                'message' => "create product failure"
+            ]);
+        }
 
-        return response()->json([
-            'message' => "succesfully uploaded"
-        ]);
+
+
+
     }
 
     public function list(){
-        $user = UserRegister::all('id_number','person_name','date_of_birth','contact_number','age','address'
+        error_log("..............");
+        $users = UserRegister::all('id_number','person_name','date_of_birth','contact_number','age','address'
         ,'religion','nationality','image_path');
 
-        return $user;
+        return $users;
     }
 
     public function search($search){
 
+        error_log($search);
         return UserRegister::where("person_name","like","%".$search."%")->
         orWhere("address","like","%".$search."%")->
         orWhere("id_number", "like", "%".$search."%")->
@@ -53,7 +80,24 @@ class UserRegisterController extends Controller
         ->get();
     }
 
-    public function dashboard(){
+    public function userReligion(){
+        
+        $usersReligion = DB::table('user_registers')->select(DB::raw('count(*) as user_count, religion'))->groupBy('religion')->get();
 
+        return $usersReligion;
+    }
+
+    public function userAge(){
+
+        $userAge = DB::table('user_registers')->select(DB::raw('count(*) as user_count, age'))->groupBy('age')->get();
+
+        return $userAge;
+    }
+
+    public function userBirthMonth(){
+
+        $userMonth = DB::table('user_registers')->select(DB::raw('count(*) as user_count, date_of_birth'))->groupBy('age')->get();
+
+        return $userMonth;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserRegisterController;
 
+
+// header('Access-Control-Allow-Origin:  *');
+// header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +18,11 @@ use App\Http\Controllers\UserRegisterController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('register', [UserRegisterController::class, "Register"]);
+Route::post('register', [UserRegisterController::class, "register"]);
 Route::get('get', [UserRegisterController::class, "list"]);
 Route::get('search/{search}', [UserRegisterController::class, "search"]);
+Route::get('user-religion', [UserRegisterController::class, "userReligion"]);
+Route::get('user-age', [UserRegisterController::class, "userAge"]);
+Route::get('user-month', [UserRegisterController::class, "userMonth"]);
+
 
